790b7d555c871f10850df60110dbd980c702156e
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2025-03-26 17:19:54 +0530

    Added the solutions to assignment 2 of OOPWC

---
+++ Assignment2/Q1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BankAccount {
+private:
+    string name;
+    int accountNumber;
+    string accountType;
+    double balance;
+
+public:
+    void initialize(string depositorName, int accountNo, string type, double initialBalance) {
+        name = depositorName;
+        accountNumber = accountNo;
+        accountType = type;
+        balance = initialBalance;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            cout << "Insufficient balance!" << endl;
+        }
+    }
+
+    void display() {
+        cout << "Account Holder: " << name << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount account;
+    account.initialize("John Doe", 12345, "Savings", 1000.0);
+    account.deposit(500.0);
+    account.withdraw(300.0);
+    account.display();
+    return 0;
+}
